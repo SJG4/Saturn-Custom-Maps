869089e53dee5e27e41851f61e4a73a4d9f62a89
SJG4 <73476067+SJG4@users.noreply.github.com>
2023-08-26 12:01:06 -0400

    Added The Bank
    Signed-off-by: SJG4 <73476067+SJG4@users.noreply.github.com>

---
+++ levels/jrb/texture_include.inc.c
@@ -0,0 +1,65 @@
+Gfx jrb_dl_custom_rocks_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 jrb_dl_custom_rocks_rgba16[] = {
+	#include "levels/jrb/custom_rocks.rgba16.inc.c"
+};
+
+Gfx jrb_dl_custom_metal_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 jrb_dl_custom_metal_rgba16[] = {
+	#include "levels/jrb/custom_metal.rgba16.inc.c"
+};
+
+Gfx jrb_dl_custom_rug_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 jrb_dl_custom_rug_rgba16[] = {
+	#include "levels/jrb/custom_rug.rgba16.inc.c"
+};
+
+Gfx jrb_dl_custom_dirt_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 jrb_dl_custom_dirt_rgba16[] = {
+	#include "levels/jrb/custom_dirt.rgba16.inc.c"
+};
+
+Gfx jrb_dl_custom_flowers_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 jrb_dl_custom_flowers_rgba16[] = {
+	#include "levels/jrb/custom_flowers.rgba16.inc.c"
+};
+
+Gfx jrb_dl_custom_wall_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 jrb_dl_custom_wall_rgba16[] = {
+	#include "levels/jrb/custom_wall.rgba16.inc.c"
+};
+
+Gfx jrb_dl_custom_road_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 jrb_dl_custom_road_rgba16[] = {
+	#include "levels/jrb/custom_road.rgba16.inc.c"
+};
+
+Gfx jrb_dl_custom_white_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 jrb_dl_custom_white_rgba16[] = {
+	#include "levels/jrb/custom_white.rgba16.inc.c"
+};
+
+Gfx jrb_dl_custom_green_blocks_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 jrb_dl_custom_green_blocks_rgba16[] = {
+	#include "levels/jrb/custom_green_blocks.rgba16.inc.c"
+};
+
+Gfx jrb_dl_custom_wood_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 jrb_dl_custom_wood_rgba16[] = {
+	#include "levels/jrb/custom_wood.rgba16.inc.c"
+};
+
+Gfx jrb_dl_custom_grass_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 jrb_dl_custom_grass_rgba16[] = {
+	#include "levels/jrb/custom_grass.rgba16.inc.c"
+};
+
+Gfx jrb_dl_custom_brick_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 jrb_dl_custom_brick_rgba16[] = {
+	#include "levels/jrb/custom_brick.rgba16.inc.c"
+};
+
+Gfx jrb_dl_custom_water_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 jrb_dl_custom_water_rgba16[] = {
+	#include "levels/jrb/custom_water.rgba16.inc.c"
+};
+
